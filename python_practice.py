39a179e866b032cc5ab851a4bb4ad353d719c98a
rggamiao <rejicamiling@gmail.com>
2024-10-03 17:07:21 -0700

    syntax error

--- python_practice.py
+++ python_practice.py
@@ -1,11 +1,12 @@
 def pascal(n):
+    triangle = []  
     for i in range(n):
         row = [1]  
         if i > 0:  
             last_row = triangle[-1]
             for j in range(len(last_row) - 1):
                 row.append(last_row[j] + last_row[j + 1])
-            row.append(1) #append(len)
+            row.append(1)  
         triangle.append(row)
         print(' '.join(map(str, row)))
 
