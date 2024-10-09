33292fcdf36b68b4d4972a6af66de7cabfbc6e25
rggamiao <rejicamiling@gmail.com>
2024-10-09 09:48:11 -0700

    getting started on additional parts

--- python_practice.py
+++ python_practice.py
@@ -1,3 +1,14 @@
+# Write a Python function called max_num()to find the Max of three numbers.
+
+def max_num(a,b,c):
+  return max([a,b,c])
+
+print(max_num(1,2,3))
+print(max_num(100,50,1))
+print(max_num(15,30,2))
+
+
+
 def pascal(n):
     triangle = []  
     for i in range(n):
